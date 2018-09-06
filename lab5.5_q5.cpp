c495c0565885fb614f8c08a3d60dc8c982a9b9bf
AnirudhJk <42132516+AnirudhJk@users.noreply.github.com>
2018-09-06 19:58:58 +0530

    Add files via upload

---
+++ lab5.5_q5.cpp
@@ -0,0 +1,22 @@
+
+//define library
+#include<iostream>
+using namespace std;
+//main programme
+int main()
+{
+int n,i,j,k;
+//Taking input from user
+cout<<"Please enter the size of rhombus size pattern"<<endl;
+cin>>n;
+for(i=0;i<n;i++)
+{
+{for (k=i;k<=n;k++)
+cout <<" ";}
+{for (j=0;j<n;j++)
+cout <<"*";}
+
+cout<<endl;
+}	
+ return 0;
+ }
